23324b072ac7cac1e0089a1460d161360ae55bdf
Wataru Nakata <wataru9871@gmail.com>
2022-04-03 20:52:56 +0900

    added checkpoint download script

---
+++ fairseq_checkpoints/download_checkpoints.sh
@@ -0,0 +1,9 @@
+wget https://msranlcmtteamdrive.blob.core.windows.net/share/wavlm/WavLM-Base.pt?sv=2020-04-08&st=2021-11-05T00%3A35%3A31Z&se=2022-11-06T00%3A35%3A00Z&sr=b&sp=r&sig=JljnRVzyHY6AjHzhVmHV5KyQQCvvGfgp9D2M02oGJBU%3D
+wget https://msranlcmtteamdrive.blob.core.windows.net/share/wavlm/WavLM-Base+.pt?sv=2020-04-08&st=2021-11-05T00%3A34%3A47Z&se=2022-10-06T00%3A34%3A00Z&sr=b&sp=r&sig=Gkf1IByHaIn1t%2FVEd9D6WHjZ3zu%2Fk5eSdoj21UytKro%3D
+wget https://msranlcmtteamdrive.blob.core.windows.net/share/wavlm/WavLM-Large.pt?sv=2020-08-04&st=2021-11-22T10%3A03%3A53Z&se=2022-11-23T10%3A03%3A00Z&sr=b&sp=r&sig=3kB8dwTCyIS8YQ7gW5oXmDrXV%2FAaLmoxBS37oPpFsz4%3D
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_small.pt
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_vox_new.pt
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/w2v_large_lv_fsh_swbd_cv.pt
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/xlsr_53_56k.pt
+wget https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt
+wget https://dl.fbaipublicfiles.com/hubert/hubert_large_ll60k.pt
